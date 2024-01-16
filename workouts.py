b303be962fa0de554acc022e2031e376253dd7ae
AshinrajM <ashinrajeevank@outlook.com>
2024-01-16 11:20:40 +0530

    string questions

--- workouts.py
+++ workouts.py
@@ -11,3 +11,24 @@
 # s=" ".join([i[1:][::-1] for i in a])
 
 # ====>iam ashin from kannur
+
+
+# Q2 => change the first letter of each word to uppercase
+# value = "usman am here go"
+
+# words=value.split()
+# uppercase =  " ".join([word.title() for word in words])
+# print(uppercase)
+
+# ==>Usman Am Here Go
+
+
+
+# Q3 => sort the array in the order of length of the words
+values = ['absd','df','sad','k']
+
+for i in range(len(values)-1):
+    if len(values[i])< len(values[i+1]):
+        values[i],values[i+1]=values[i+1],values[i]
+
+print(values)
