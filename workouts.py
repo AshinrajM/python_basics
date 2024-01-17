d8df24e82f37fd10f576cc4509b0733dcca1044c
AshinrajM <ashinrajeevank@outlook.com>
2024-01-17 09:04:29 +0530

    questtion 4

--- workouts.py
+++ workouts.py
@@ -25,10 +25,32 @@
 
 
 # Q3 => sort the array in the order of length of the words
-values = ['absd','df','sad','k']
+# values = ['absd','df','sad','k']
 
-for i in range(len(values)-1):
-    if len(values[i])< len(values[i+1]):
-        values[i],values[i+1]=values[i+1],values[i]
+# for i in range(len(values)-1):
+#     if len(values[i])< len(values[i+1]):
+#         values[i],values[i+1]=values[i+1],values[i]
 
-print(values)
+# print(values)
+
+
+#Q4 => create string that contains digits and letters that are not in the given string.
+#   ( in the result string digits has to be at first and letters after that both should be in sorted order)
+# s='7985interdisciplinary12'
+
+# n=['0','1','2','3','4','5','6','7','8','9']
+# l=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# digit=[]
+# letter=[]
+# for i in n:
+#     if i not in s:
+#         digit.append(i)
+# for i in l:
+#     if i not in s:
+#         letter.append(i)
+# result=digit+letter
+# print(''.join(result))
+
+
+
+#Q
